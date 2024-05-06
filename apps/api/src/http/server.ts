96aedcaa9ef24a61fcd8ee9a735a92343ad112e9
Ian <ssianfilipe@gmail.com>
2024-05-06 10:42:54 -0300

    feat: setup swagger with fastify and zod

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,7 +1,9 @@
 import cors from '@fastify/cors'
+import swagger from '@fastify/swagger'
+import swaggerUI from '@fastify/swagger-ui'
 import fastify from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -13,6 +15,22 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(swagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS app with multi-tenant and RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(swaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(cors)
 
