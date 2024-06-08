55eebf92527f3c8d5887aa6ca0b552f5fea39aa2
Ian <ssianfilipe@gmail.com>
2024-06-08 16:59:33 -0300

    create sign up and recover password pages

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -8,9 +8,13 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">Name</Label>
+        <Input id="name" name="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
         <Input id="email" name="email" type="email" />
@@ -18,27 +22,29 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input id="password" name="password" type="password" />
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm password</Label>
+        <Input
+          id="password_confirmation"
+          name="password_confirmation"
+          type="password"
+        />
       </div>
       <Button type="submit" className="w-full">
-        Sign in
+        Sign up
       </Button>
       <Button type="button" variant="link" size="sm" className="w-full" asChild>
-        <Link href="/auth/sign-up">Create an account</Link>
+        <Link href="/auth/sign-in">Already have an account? Sign in</Link>
       </Button>
       <Separator />
       <Button type="button" variant="outline" className="w-full">
         <Image src={githubIcon} className="mr-2 size-4 dark:invert" alt="" />
-        Sign in with GitHub
+        Sign up with GitHub
       </Button>
       <Button type="button" variant="outline" className="w-full">
         <Image src={googleIcon} className="mr-2 size-4" alt="" />
-        Sign in with Google
+        Sign up with Google
       </Button>
     </form>
   )
