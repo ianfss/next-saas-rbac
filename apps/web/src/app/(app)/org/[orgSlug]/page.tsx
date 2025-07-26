2ca42c6abb9145d0f1f2af210d4445e8d6d9d67a
Ian <ssianfilipe@gmail.com>
2025-07-26 00:27:15 -0300

    tabs navigation

--- apps/web/src/app/(app)/org/[orgSlug]/page.tsx
+++ apps/web/src/app/(app)/org/[orgSlug]/page.tsx
@@ -1,10 +1,7 @@
-import { Header } from '@/components/header'
-
 export default async function Projects() {
   return (
     <div className="py-4 space-y-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4"></main>
+      <h1 className="text-2xl font-bold">Projects</h1>
     </div>
   )
 }
